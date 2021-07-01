3dd393b7d286d76fcb5218a57234c7593fb230ca
cglatot <ciaran@thegallagherclan.co.uk>
2021-07-01 15:55:52 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,8 +7,3 @@ output "master_version" {
   description   = "Current master kubernetes version"
   value         = google_container_cluster.default.master_version
 }
-
-output "cluster_id" {
-  description   = "ID of the cluster"
-  value         = google_container_cluster.default.cluster_id
-}
